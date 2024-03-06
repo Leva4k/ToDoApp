68378c0c4410e96dd523f319b81534f981420c72
Leva4k <levanax@mail.ru>
2024-03-06 20:46:18 +0500

    first commit

---
+++ TodoApp.API/TodoApp.API/TodoApp.API/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.API.Models
+{
+    public class Todo
+    {
+        public  Guid Id { get; set; }
+        public  string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsComleted { get; set; }
+        public DateTime? CompletedDate { get; set; }    
+    }
+}
